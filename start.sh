fee01a5a1af2ae8f95730c8d96335e71fe50110f
unknown <antoine.cuvilliez@outlook.fr>
2025-03-28 14:28:35 +0100

    feat(deployment) Adding sh start and stop

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Vérifier si Minikube est lancé
+echo "Checking if Minikube is running..."
+if ! minikube status | grep -q "Running"; then
+  echo "Starting Minikube..."
+  minikube start --driver=docker --cpus=4 --memory=4096
+else
+  echo "Minikube is already running."
+fi
+
+# Appliquer les CRDs de Traefik
+echo "Applying Traefik CRDs..."
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.0/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+# Vérifier si un paramètre est fourni
+if [ -z "$1" ]; then
+  echo "No pod specified. Defaulting to deploying all pods..."
+  PROFILES="restaurateur"
+else
+  PROFILES="$1"
+fi
+
+# Lancer Skaffold avec les profils
+
+echo "Starting Skaffold for profiles: $PROFILES..."
+skaffold dev -p "$PROFILES"
